04164e3f8e073d25e46cd93ffb463f2323665668
tkach <arttka309205@gmail.com>
2025-09-25 13:10:21 +0300

    init

--- ex2/2.py
+++ ex2/2.py
@@ -14,6 +14,16 @@ blurred_9x9 = cv.GaussianBlur(my_image,(9, 9), 0)
 double_blurred_5x5 = cv.GaussianBlur(blurred_5x5, (5, 5), 0)
 
 
+cv.namedWindow("5x5 Gaussian", cv.WINDOW_NORMAL)
+cv.resizeWindow("5x5 Gaussian", 300, 300)
+
+cv.namedWindow("9x9 Gaussian", cv.WINDOW_NORMAL)
+cv.resizeWindow("9x9 Gaussian", 300, 300)
+
+cv.namedWindow("5x5 Gaussian Double", cv.WINDOW_NORMAL)
+cv.resizeWindow("5x5 Gaussian Double", 300, 300)
+
+
 cv.imshow("5x5 Gaussian", blurred_5x5)
 cv.imshow("9x9 Gaussian", blurred_9x9)
 cv.imshow("5x5 Gaussian Double", double_blurred_5x5)
--- ex3/3.py
+++ ex3/3.py
@@ -7,6 +7,14 @@ my_image = cv.imread('3a.jpg')
 a1 = cv.GaussianBlur(my_image,(9, 9), 1)
 a2 = cv.GaussianBlur(my_image,(9, 9), 4)
 a3 = cv.GaussianBlur(my_image,(9, 9), 6)
+
+# cv.namedWindow("a1", cv.WINDOW_NORMAL)
+# cv.resizeWindow("a1", 600, 700)
+# cv.namedWindow("a2", cv.WINDOW_NORMAL)
+# cv.resizeWindow("a2", 600, 700)
+# cv.namedWindow("a3", cv.WINDOW_NORMAL)
+# cv.resizeWindow("a3", 600, 700)
+#
 # cv.imshow("a1", a1)
 # cv.imshow("a2", a2)
 # cv.imshow("a3", a3)
@@ -16,6 +24,14 @@ a3 = cv.GaussianBlur(my_image,(9, 9), 6)
 b1 = cv.GaussianBlur(my_image,(0, 0), 1)
 b2 = cv.GaussianBlur(my_image,(0, 0), 4)
 b3 = cv.GaussianBlur(my_image,(0, 0), 6)
+
+# cv.namedWindow("b1", cv.WINDOW_NORMAL)
+# cv.resizeWindow("b1", 600, 700)
+# cv.namedWindow("b2", cv.WINDOW_NORMAL)
+# cv.resizeWindow("b2", 600, 700)
+# cv.namedWindow("b3", cv.WINDOW_NORMAL)
+# cv.resizeWindow("b3", 600, 700)
+#
 # cv.imshow("b1", b1)
 # cv.imshow("b2", b2)
 # cv.imshow("b3", b3)
@@ -23,21 +39,37 @@ b3 = cv.GaussianBlur(my_image,(0, 0), 6)
 
 # c
 c1 = cv.GaussianBlur(my_image, (0, 0), sigmaX=1, sigmaY=9)
+
+# cv.namedWindow("c1", cv.WINDOW_NORMAL)
+# cv.resizeWindow("c1", 600, 700)
+#
 # cv.imshow("c1", c1)
 
 
 # d
 d1 = cv.GaussianBlur(my_image, (0, 0), sigmaX=9, sigmaY=1)
+
+# cv.namedWindow("d1", cv.WINDOW_NORMAL)
+# cv.resizeWindow("d1", 600, 700)
+#
 # cv.imshow("d1", d1)
 
 
 # e
 e1 = cv.GaussianBlur(c1, (0, 0), sigmaX=9, sigmaY=1)
+
+# cv.namedWindow("e1", cv.WINDOW_NORMAL)
+# cv.resizeWindow("e1", 600, 700)
+#
 # cv.imshow("e1", e1)
 
 
 # f
 f1 = cv.GaussianBlur(my_image, (9, 9), sigmaX=0, sigmaY=0)
+
+# cv.namedWindow("f1", cv.WINDOW_NORMAL)
+# cv.resizeWindow("f1", 600, 700)
+#
 # cv.imshow("f1", f1)
 
 
